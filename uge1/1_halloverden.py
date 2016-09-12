090fc8fcbd265c48795e6804ea6a2f80b241770a
Ragnar <ragnar.reynisson@gmail.com>
2016-09-12 18:20:28 +0200

    Uge 1 - skelet
    Skelettet for Python forløb Uge 1.  Filer for at demonstrere udprintning
    mv.

---
+++ uge1/1_halloverden.py
@@ -0,0 +1,19 @@
+#coding=utf8
+# Pythonundervisning Coding Pirates Tårnby
+# Første gang - basisting
+
+# <-- Dette bogstav gør at alt det der står efter det bliver ignoreret
+# Det betyder at vi kan skrive kommentarer i vores kode
+# Det er ret smart når andre skal læse hvad vi koder, så kan vi forklare hvad der sker
+
+# Det første vi vil gøre er at skrive til skærmen
+# Så skal vi bruge udskrivningsfunktionen "print"
+# En funktion er en kommando efterfulgt af parenteser
+# Inden i parenteserne står der de ting som funktionen skal bruge
+# Senere vil vi lære jer at lave jeres egen funktioner
+
+print("Hallo verden!")
+
+# Prøv at trykke på F10 mens I holder Shift nede.  I vinduet nedenfor vil I nu se at vi printer noget ud til skærmen.
+
+# Øvelse:  Skriv jeres navn ud til skærmen ved at bruge print-kommandoen
